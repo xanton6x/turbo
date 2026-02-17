898bd1241a0d3e51021e4984d99b5987e8473249
xanton6x <toha400@gmail.com>
2026-02-17 17:20:09 +0000

    asd

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDiCZR0dPnvjcxajI6fswQot0z4SSMiDI0",
+  authDomain: "turbo-f5f6d.firebaseapp.com",
+  projectId: "turbo-f5f6d",
+  storageBucket: "turbo-f5f6d.firebasestorage.app",
+  messagingSenderId: "778985009405",
+  appId: "1:778985009405:web:85b798b9c97d581822ce94"
 };
 
 const app = initializeApp(firebaseConfig);
